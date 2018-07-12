9ae4534beb312a8be58b4322923ea32c695dc5c0
Francesco Ciocchetti <fciocchetti@mintel.com>
2018-07-12 13:38:27 +0100

    Use curl retry option

--- scripts/start-local-routing.sh
+++ scripts/start-local-routing.sh
@@ -5,16 +5,9 @@ set -ex
 export NODE_NAME=${NODE_NAME:-${HOSTNAME}}
 
 
-count=1
-while [ $count -le 5 ]; do
-  (( count++ ))
-  CLUSTER_SETTINGS=$(curl --connect-timeout 3 -s -XGET "http://${DISCOVERY_SERVICE}:9200/_cluster/settings")
-  if echo "${CLUSTER_SETTINGS}" | grep -E "${NODE_NAME}"; then
-    echo "Activate node ${NODE_NAME}"
-    curl --connect-timeout 3 -s -XPUT -H 'Content-Type: application/json' "http://${DISCOVERY_SERVICE}:9200/_cluster/settings" -d "{ \"transient\" :{ \"cluster.routing.allocation.exclude._name\" : null }}"
-    if [ $? -eq 0 ]; then
-      break
-    fi
-  fi
-  sleep 10
-done
+curl --retry 6 --connect-timeout 3 -s -XGET "http://${DISCOVERY_SERVICE}:9200/_cluster/settings" 
+CLUSTER_SETTINGS=$(curl --connect-timeout 3 -s -XGET "http://${DISCOVERY_SERVICE}:9200/_cluster/settings")
+if echo "${CLUSTER_SETTINGS}" | grep -E "${NODE_NAME}"; then
+  echo "Activate node ${NODE_NAME}"
+  curl --retry 3 --connect-timeout 3 -s -XPUT -H 'Content-Type: application/json' "http://${DISCOVERY_SERVICE}:9200/_cluster/settings" -d "{ \"transient\" :{ \"cluster.routing.allocation.exclude._name\" : null }}"
+fi
--- scripts/stop-local-routing.sh
+++ scripts/stop-local-routing.sh
@@ -5,11 +5,11 @@ set -ex
 export NODE_NAME=${NODE_NAME:-${HOSTNAME}}
 
 echo "Disabling Local Routing for this Node - Will move all shards to other nodes"
-curl -s -XPUT -H 'Content-Type: application/json' 'localhost:9200/_cluster/settings' -d "{ \"transient\" :{ \"cluster.routing.allocation.exclude._name\" : \"${NODE_NAME}\" }}"
+curl --retry 3 -s -XPUT -H 'Content-Type: application/json' 'localhost:9200/_cluster/settings' -d "{ \"transient\" :{ \"cluster.routing.allocation.exclude._name\" : \"${NODE_NAME}\" }}"
 
 while true ; do
   echo -e "Wait for node ${NODE_NAME} to become empty"
-  SHARDS_ALLOCATION=$(curl -s -XGET 'http://localhost:9200/_cat/shards')
+  SHARDS_ALLOCATION=$(curl --retry 3 -s -XGET 'http://localhost:9200/_cat/shards')
   if ! echo "${SHARDS_ALLOCATION}" | grep -E "${NODE_NAME}"; then
     break
   fi
