f139b31ecbdc5dffb2d43c1d0c3f69590407a212
Francesco Ciocchetti <fciocchetti@mintel.com>
2018-07-12 12:22:29 +0100

    Rety if master is not available

--- scripts/start-local-routing.sh
+++ scripts/start-local-routing.sh
@@ -4,8 +4,17 @@ set -ex
 
 export NODE_NAME=${NODE_NAME:-${HOSTNAME}}
 
-CLUSTER_SETTINGS=$(curl -s -XGET "http://${DISCOVERY_SERVICE}:9200/_cluster/settings")
-if echo "${CLUSTER_SETTINGS}" | grep -E "${NODE_NAME}"; then
-  echo "Activate node ${NODE_NAME}"
-  curl -s -XPUT -H 'Content-Type: application/json' "http://${DISCOVERY_SERVICE}:9200/_cluster/settings" -d "{ \"transient\" :{ \"cluster.routing.allocation.exclude._name\" : null }}"
-fi
+
+count=1
+while [ $count -le 5 ]; do
+  (( count++ ))
+  CLUSTER_SETTINGS=$(curl --connect-timeout 3 -s -XGET "http://${DISCOVERY_SERVICE}:9200/_cluster/settings")
+  if echo "${CLUSTER_SETTINGS}" | grep -E "${NODE_NAME}"; then
+    echo "Activate node ${NODE_NAME}"
+    curl --connect-timeout 3 -s -XPUT -H 'Content-Type: application/json' "http://${DISCOVERY_SERVICE}:9200/_cluster/settings" -d "{ \"transient\" :{ \"cluster.routing.allocation.exclude._name\" : null }}"
+    if [ $? -eq 0 ]; then
+      break
+    fi
+  fi
+  sleep 10
+done
